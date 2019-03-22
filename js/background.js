7aa3d59294e72b376e5fb6b7e168cca3606044dc
matico <mactiencong@gmail.com>
2019-03-22 23:32:36 +0900

    First commit

---
+++ js/background.js
@@ -0,0 +1,25 @@
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details) {
+        if(isFacebookAds(details)){
+            console.log('Blocked: ', details.url)
+            return {cancel: true}
+        }
+    },
+    {urls: ["<all_urls>"]},
+    ["blocking"]
+);
+const url = chrome.runtime.getURL('../data/ads_urls.json');
+let ads_urls = []
+fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+        ads_urls = data
+    });
+function isFacebookAds(details){
+    for (let index = 0; index < ads_urls.length; index++) {
+        const urlPattern = ads_urls[index]
+        const isFacebookAdsUrl = details.url.indexOf(urlPattern) !== -1
+        if(isFacebookAdsUrl) return true
+    }
+    return false
+}
---
+++ js/contentScript.js
@@ -0,0 +1,4 @@
+var ads_divs = document.querySelectorAll("div.ego_section_container");
+ads_divs.forEach(function(element) {
+    element.remove();
+});
